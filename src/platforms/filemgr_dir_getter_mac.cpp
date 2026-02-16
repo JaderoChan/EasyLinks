1290e8c3303ae728427aa4ff5b3da14ce3eae146
JaderoChan <c_dl_cn@outlook.com>
2026-02-16 21:46:19 +0800

    分离MacOS上的逻辑代码与脚本文件

--- src/platforms/filemgr_dir_getter_mac.cpp
+++ src/platforms/filemgr_dir_getter_mac.cpp
@@ -4,14 +4,17 @@
 #include <stdexcept>
 #include <string>
 
+#include <qfile.h>
 #include <qfileinfo.h>
 
-static std::string runCommand(const char* cmd)
+#include "config.h"
+
+static std::string runCommand(const std::string& cmd)
 {
     char buf[256] = {0};
     std::string out;
 
-    FILE* pipe = popen(cmd, "r");
+    FILE* pipe = popen(cmd.c_str(), "r");
     if (!pipe)
         throw std::runtime_error("Failed to open pipe");
     while (fgets(buf, sizeof(buf), pipe))
@@ -23,33 +26,10 @@ static std::string runCommand(const char* cmd)
 
 QString getFocusedFileManagerDir()
 {
-    const char* script = R"(
-        tell application "System Events"
-            set frontApp to name of first application process where it is frontmost
-        end tell
-
-        if frontApp is not "Finder" then
-            error "Front application is not Finder"
-        end if
-
-        tell application "Finder"
-            if (count of Finder windows) = 0 then
-                error "No Finder window is open"
-            end if
-
-            set targetFolder to (quoted form of POSIX path of (target of front window as alias))
-            if targetFolder is missing value then
-                error "Finder window has no valid target"
-            end if
-
-            return targetFolder
-        end tell
-    )";
-
-    std::string cmd = "osascript -e '";
-    cmd.append(script);
-    cmd.append("'");
-    std::string out = runCommand(cmd.c_str());
+    static const std::string script =
+        QFile(APP_RESOURCE_DIRPATH + "scripts/get_focused_finder_dir.sh").readAll().toStdString();
+    static const std::string cmd = "osascript -e '" + script + "'";
+    std::string out = runCommand(cmd);
 
     QString path = QString::fromStdString(out);
     // 去除首尾空白字符。
