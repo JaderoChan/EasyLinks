c67994c57b4ffe785469ad0a5657c90d1e74d52d
JaderoChan <c_dl_cn@outlook.com>
2026-02-19 18:36:55 +0800

    update cleanup script

--- cleanup_register.bat
+++
@@ -1,6 +0,0 @@
-:REM 用于清理程序注册表条目（Used for cleaning up program registry entries）
-
-@echo off
-reg delete "HKCU\Software\JaderoChan\EasyLinks" /f
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "EasyLins" /f
-pause
