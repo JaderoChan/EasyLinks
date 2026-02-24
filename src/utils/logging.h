009350413482e75c5a97c62f41a8e93ffdebfd59
JaderoChan <c_dl_cn@outlook.com>
2026-02-24 10:05:06 +0800

    update

--- src/utils/logging.h
+++ src/utils/logging.h
@@ -4,7 +4,7 @@
 #include <qstring.h>
 
 template <typename IOType, typename ...Args>
-void qout(IOType io, const char *formatStr, Args &&...args)
+void qout(IOType io, const char* formatStr, Args&& ...args)
 {
     QString str(formatStr);
     ((str = str.arg(std::forward<Args>(args))), ...);
